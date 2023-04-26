cf7ed53f11277aa6f86daa7bf41ea84048712214
Dhruv Patel <dhruv225@outlook.com>
2023-04-26 15:00:56 -0400

    created cards data with temp info

---
+++ src/components/CardData.ts
@@ -0,0 +1,54 @@
+export const Cards = [
+    {
+        id: 1,
+        title: "Card 1",
+        img: "/example.com/image1.jpg",
+
+    },
+    {
+        id: 2,
+        title: "Card 2",
+        img: "/example.com/image2.jpg",
+    },
+    {
+        id: 3,
+        title: "Card 3",
+        img: "/example.com/image3.jpg",
+    },
+    {
+        id: 4,
+        title: "Card 4",
+        img: "/example.com/image4.jpg",
+    },
+    {
+        id: 5,
+        title: "Card 5",
+        img: "/example.com/image5.jpg",
+    },
+    {
+        id: 6,
+        title: "Card 6",
+        img: "/example.com/image6.jpg",
+    },
+    {
+        id: 7,
+        title: "Card 7",
+        img: "/example.com/image7.jpg",
+    },
+    {
+        id: 8,
+        title: "Card 8",
+        img: "/example.com/image8.jpg",
+    },
+    {
+        id: 9,
+        title: "Card 9",
+        img: "/example.com/image9.jpg",
+    },
+    {
+        id: 10,
+        title: "Card 10",
+        img: "/example.com/image10.jpg",
+    },
+    
+]
