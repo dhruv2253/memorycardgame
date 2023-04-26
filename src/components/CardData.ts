1bbb8df2f858b3cd72812b5e2bb6d1847673ecf9
Dhruv Patel <dhruv225@outlook.com>
2023-04-26 15:05:52 -0400

    added click prop to cards

--- src/components/CardData.ts
+++ src/components/CardData.ts
@@ -3,52 +3,61 @@ export const Cards = [
         id: 1,
         title: "Card 1",
         img: "/example.com/image1.jpg",
-
+        clicked: false
     },
     {
         id: 2,
         title: "Card 2",
         img: "/example.com/image2.jpg",
+        clicked: false
     },
     {
         id: 3,
         title: "Card 3",
         img: "/example.com/image3.jpg",
+        clicked: false
     },
     {
         id: 4,
         title: "Card 4",
         img: "/example.com/image4.jpg",
+        clicked: false
     },
     {
         id: 5,
         title: "Card 5",
         img: "/example.com/image5.jpg",
+        clicked: false
     },
     {
         id: 6,
         title: "Card 6",
         img: "/example.com/image6.jpg",
+        clicked: false
     },
     {
         id: 7,
         title: "Card 7",
         img: "/example.com/image7.jpg",
+        clicked: false
     },
     {
         id: 8,
         title: "Card 8",
         img: "/example.com/image8.jpg",
+        clicked: false
     },
     {
         id: 9,
         title: "Card 9",
         img: "/example.com/image9.jpg",
+        clicked: false
     },
     {
         id: 10,
         title: "Card 10",
         img: "/example.com/image10.jpg",
-    },
-    
+        clicked: false
+    }
+
 ]
