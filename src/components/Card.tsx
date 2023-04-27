f82a7684620c9ed45d61326f88e8fd7a423fb1f3
Dhruv Patel <dhruv225@outlook.com>
2023-04-27 16:59:17 -0400

    added gameOver method + changed class

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,6 +5,7 @@ type CardProps = {
     img: string;
     clicked: boolean;
    updateScore: () => void;
+   gameOver: () => void;
 }
 function Card(props: CardProps) {
     const [clicked, setClicked] = useState(false);
@@ -14,11 +15,16 @@ function Card(props: CardProps) {
             setClicked(true);
             console.log('clicked', props.title)
             props.updateScore();
+        } else {
+            props.gameOver();
+            setClicked(false);
+           
         }
+        
       
     }
     return (
-        <div className='card' onClick={handleClick}>
+        <div className={`card ${props.clicked ? 'clicked' : ''}`} onClick={handleClick}>
             <img className='card-image' src={`../images/${props.img}`} alt={props.img} />
             <span className="name">{props.title}</span>
         </div>
