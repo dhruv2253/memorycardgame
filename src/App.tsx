430d4fb93bf42f331188a6d0177da7764851ad96
Dhruv Patel <dhruv225@outlook.com>
2023-04-27 00:26:59 -0400

    map card data to card components

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Scores from "./components/Scores";
+import "./App.css"
+import Card from "./components/Card";
+import {cardData} from "./CardData";
+
+function App() {
+    const cards = cardData.map (card => {
+        return (
+            <Card 
+            title={card.title} 
+            img={card.img} 
+            clicked={card.clicked} />
+        )
+    })
+    return (
+
+        <div>
+             <Scores currentScore={0} highScore={0} />
+             <div className="card-board">
+                {cards}
+             </div>
+            
+        </div>
+    )
+   
+
+}
+
+export default App;
