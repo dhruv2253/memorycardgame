3cc96e17df787e711db6198b8b72c6cd0288f88a
Dhruv Patel <dhruv225@outlook.com>
2023-04-27 22:20:16 -0400

    moved images to public + fixed path

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -25,7 +25,7 @@ function Card(props: CardProps) {
     }
     return (
         <div className={`card ${props.clicked ? 'clicked' : ''}`} onClick={handleClick}>
-            <img className='card-image' src={`../images/${props.img}`} alt={props.img} />
+            <img className='card-image' src={`/images/${props.img}`} alt={props.img} />
             <span className="name">{props.title}</span>
         </div>
     );
