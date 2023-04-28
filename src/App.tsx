fb323b46286052666c535ca6cec117932711820d
Dhruv Patel <dhruv225@outlook.com>
2023-04-28 14:25:15 -0400

    Move handleclick, useState for cards, reseteffect

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,74 @@ import Scores from "./components/Scores";
 import "./App.css"
 import Card from "./components/Card";
 import {cardData} from "./CardData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
     const [score, setScore] = useState(0);
     const [highScore, setHighScore] = useState(0);
+    /** Store cards in state */
+    const [cards, setCards] = useState(cardData);
     
+    /** Handles the click event for the card with the given title */
+    const handleClick = (title: string) => {
+        // Find the card with the given title
+        const card = cards.find(card => card.title === title);
+        // If the card is not found, return
+        if (!card) {
+            return;
+        }
+        // If the card is not clicked, set it to clicked and update the score
+        if (!card.clicked){
+            card.clicked = true;
+            console.log('clicked', title)
+            updateScore();
+        } else {
+            // If the card is clicked, reset the score and the cards
+            gameOver();
+           
+        }
+    }
 
     const updateScore = () => {
         setScore(score + 1);
+        
+           
     }
     const gameOver = () => {
+        
         if (score > highScore) {
             setHighScore(score);
         }
         setScore(0);
-        cardData.forEach((card) => {
-            card.clicked = false;
-          });
+       
     }
 
-    const cards = cardData.map (card => {
- 
+    /** Reset the cards when the score is 0 */
+    useEffect(() => {
+        // If the score is 0, reset the cards
+        if (score === 0) {
+            // Create a new array of cards with all the cards set to not clicked
+            const newCards = cards.map(card => {
+                return {
+                    ...card,
+                    clicked : false
+                }
+            })
+          
+            // Set the cards to the new array
+            setCards(newCards);    
+        }
+    }, [score]);
+
+    
+    const cardComponents = cards.map(card => {
 
         return (
             <Card 
             title={card.title} 
             img={card.coverImg} 
             clicked={card.clicked}
-            updateScore={updateScore}
-            gameOver={gameOver}
+            handleClick={handleClick}
             />
         
         )
@@ -41,7 +79,7 @@ function App() {
         <div>
              <Scores currentScore={score} highScore={highScore}/>
              <div className="card-board">
-                {cards}
+                {cardComponents}
              </div>
             
         </div>
